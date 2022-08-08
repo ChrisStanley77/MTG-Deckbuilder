f03abbc73c1829f7a5ff4cbb4ae710782904581c
Dragonliver <castanley77@live.com>
2022-08-08 08:56:15 -0600

    Card Service

---
+++ Microservices/Card/Card.cs
@@ -0,0 +1,15 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+public class Card
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = null!;
+    public string Image { get; set;} = null!;
+    public string Name { get; set;} = null!;
+    public string Mana { get; set;} = null!;
+    public string Type { get; set;} = null!;
+    public string Text { get; set;} = null!;
+    public string Set { get; set;} = null!;
+}
