ff311316d6c7a8637c15b416745a366418dd4ff8
Dragonliver <castanley77@live.com>
2022-08-09 11:24:02 -0600

    Dockerfile and program file

--- Microservices/Card/Program.cs
+++ Microservices/Card/Program.cs
@@ -1,6 +1,22 @@
+using Steeltoe.Discovery.Client;
+using Settings;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddDiscoveryClient(builder.Configuration);
+builder.Services.Configure<CardDatabaseSettings>(builder.Configuration.GetSection("CardDatabase"));
+builder.Services.AddSingleton<Service.CardService>();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+        });
+});
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
+app.MapControllers();
+app.UseCors();
 app.Run();
