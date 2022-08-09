04a797d47a90d3b900c5aae689a995974a077f72
joshuaj25679 <79765022+joshuaj25679@users.noreply.github.com>
2022-08-09 08:46:57 -0600

    Changed some account stuff

--- DockerCompose/SQL_DB/SQLCreation.sql
+++ DockerCompose/SQL_DB/SQLCreation.sql
@@ -1,6 +1,6 @@
 /****** Script for SelectTopNRows command from SSMS  ******/
 SELECT TOP (1000) [Id]
-      ,[Email]
-      ,[Username]
-      ,[Password]
+    ,[Email]
+    ,[Username]
+    ,[Password]
   FROM [mtg_deckbuilder].[dbo].[Accounts]
