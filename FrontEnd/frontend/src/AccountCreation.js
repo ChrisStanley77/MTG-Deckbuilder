f1df719a44a4012a5c90ad86ded105b68ba837e8
Dougy1203 <78436877+Dougy1203@users.noreply.github.com>
2022-08-19 09:49:11 -0600

    Front End to Main
    This was the pathwork solution to merge since it was broken

---
+++ FrontEnd/frontend/src/AccountCreation.js
@@ -0,0 +1,73 @@
+import React from "react";
+import {useState} from "react";
+import './Account.css';
+
+function AccountCreation() {
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const handleChange = (e) => {
+        const {id, value} = e.target;
+
+        if(id === "email"){
+            setEmail(value);
+        }
+
+        if(id === "username"){
+            setUsername(value);
+        }
+
+        if(id === "password"){
+            setPassword(value);
+        }
+
+        if(id === "confirmPassword"){
+            setConfirmPassword(value);
+        }
+    }
+
+    const handleSubmit = () => {
+        console.log(email, username, password, confirmPassword);
+    }
+
+    return(
+        <div>
+            <h1 className="AccountTitle">Create Account</h1>
+            <div className="accountform">
+                <div className="form-body">
+                    <div className="email">
+                        <label className="label" >Email</label>
+                        <div>
+                            <input className="input" type={"text"} value={email} onChange= {(e) => handleChange(e)} id="email" placeholder="Email"></input>
+                        </div>
+                    </div>
+                    <div className="username">
+                        <label className="label" >Username</label>
+                        <div>
+                        <input className="input" type={"text"} value={username} onChange= {(e) => handleChange(e)} id="username" placeholder="Username"></input>
+                        </div>
+                    </div>
+                    <div className="password">
+                        <label className="label" >Password</label>
+                        <div>
+                        <input className="input" type={"password"} value={password} onChange= {(e) => handleChange(e)} id="password" placeholder="Password"></input>
+                        </div>
+                    </div>
+                    <div className="confirm-password">
+                        <label className="label" >Confirm Password</label>
+                        <div>
+                        <input className="input" type={"password"} value={confirmPassword} onChange= {(e) => handleChange(e)} id="confirmPassword" placeholder="Confirm Password"></input>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer">
+                    <button onClick={() => handleSubmit()} type="submit" className="button">Sign Up</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AccountCreation;
---
+++ FrontEnd/frontend/src/App.js
@@ -0,0 +1,84 @@
+import './App.css';
+import {useState} from "react";
+import { View, Image, StyleSheet } from 'react-native';
+import AccountCreation from './AccountCreation';
+import Login from './LoginPage';
+import DecksPage from './DecksPage';
+import Deck from './DeckCreation';
+import {BrowserRouter, Link} from "react-router-dom";
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+  },
+  Card: {
+    width: auto,
+    height: auto,
+  },
+});
+
+
+const DisplayAnImage = (props) => {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.Card}
+        source={{
+          uri: 'https://reactnative.dev/img/tiny_logo.png',
+        }}
+      />
+    </View>
+  );
+}
+
+function HomePage(){
+  const [search, setSearch] = useState("");
+  const [cards, setCards] = useState([]);
+
+  const handleChange = (e) => {
+    const {id, value} = e.target;
+
+    if(id === "search"){
+        setSearch(value);
+    }
+  }
+
+  const handleClick = () => {
+    
+    console.log(search);
+  }
+
+  return(
+    <BrowserRouter>
+      <div>
+        <h1 className='homeTitle'>Welcome to MTG Deck Builder</h1>
+        <div className='buttonLogin'>
+          <nav>
+            <Link to={"/LoginPage"}>
+              <button className='login' type='submit'>Login</button>
+            </Link>
+          </nav>
+        </div>
+        <div>      
+          <input className="search" type={"text"} value={search} onChange= {(e) => handleChange(e)} id="search" placeholder="Search"></input>
+        </div>
+        <div>
+          <button onClick={() => handleClick()} className='button' type='submit'>Search</button>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      {/* <AccountCreation/> */}
+      {/*<Login/>*/}
+      {/*<DecksPage/>*/}
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
